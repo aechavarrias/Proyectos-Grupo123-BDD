1a09b11f42f5c1e0dfc763c5d871f0ebd26394d9
JuanGalilea <juangk93@gmail.com>
2020-10-20 19:03:38 -0300

    pagina web

---
+++ Sites/config/conexion.php
@@ -0,0 +1,11 @@
+<?php
+  try {
+    #Pide las variables para conectarse a la base de datos.
+    require('data.php'); 
+    # Se crea la instancia de PDO
+    $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+    $db1 = new PDO("pgsql:dbname=$databaseName1;host=localhost;port=5432;user=$user1;password=$password1");
+  } catch (Exception $e) {
+    echo "No se pudo conectar a la base de datos: $e";
+  }
+?>
---
+++ Sites/config/data.php
@@ -0,0 +1,8 @@
+<?php
+  $user = 'grupo124';
+  $password = '2020bdd';
+  $databaseName = 'grupo124e2';
+  $user1 = 'grupo123';
+  $password1 = 'junglediff';
+  $databaseName1 = 'grupo123e2';
+?>
