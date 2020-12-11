b55c628587656eab9dcf5e2af81879765fda4d98
JuanGalilea <juangk93@gmail.com>
2020-12-11 15:59:33 -0300

    asd

--- Sites/ingreso/ingresar.php
+++ Sites/ingreso/ingresar.php
@@ -2,7 +2,7 @@
   session_start();
 
   require("../config/conexion.php");
-  
+
   $user = $_POST["pasaporte"];
   $pass = $_POST["contraseña"];
 
@@ -10,27 +10,20 @@
   $userResult = $db1 -> prepare($userQuery);
   $userResult -> execute();
   $usuario = $userResult -> fetchAll();
-
-  echo "$usuario";
-
-//   $passportQuery = "SELECT numero_pasaporte FROM usuarios WHERE numero_pasaporte LIKE '%$pasaporte%';";
-//   $passportResult = $db1 -> prepare($passportQuery);
-//   $passportResult -> execute();
-
-
-//   	#$tipo = $_POST["tipo_elegido"];
-// 	$nombre = $_POST["nombre"];
-
-//   $query = "SELECT nombre FROM personal";
-//  #WHERE tipo LIKE '%$tipo%' AND nombre LIKE '%$nombre%';";
-//  $result = $db1 -> prepare($query);
-//  $result -> execute();
-//  $personal = $result -> fetchAll();
-
-
   // include('../templates/header_inicio.html');
   // header("Location: http://google.com/");
 ?>
+
+<table>
+    <tr>
+      <th>Usuario</th>
+    </tr>
+  <?php
+	foreach ($usuario as $asd) {
+  		echo "<tr> <td>$asd[0]</td> <td>";
+	}
+  ?>
+</table>
 
 <body>
 </body>
