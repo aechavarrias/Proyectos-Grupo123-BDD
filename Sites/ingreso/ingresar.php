fd638658a64cbcea704786cca7e8755666b7a44e
JuanGalilea <juangk93@gmail.com>
2020-12-11 16:34:27 -0300

    tengo fe

--- Sites/ingreso/ingresar.php
+++ Sites/ingreso/ingresar.php
@@ -12,8 +12,12 @@
   $userResult -> execute();
   $usuario = $userResult -> fetchAll();
 
-  if ($user == $usuario[0][4]) {
-    header("Location: http://google.com/");
+  if ($user == $usuario[0][4] and $pass == $usuario[0][6]) {
+    $_SESSION["loggged"] = TRUE;
+    $_SESSION["username"] = $usuario[0][1];
+    // WEVIAR A MONGO / HEROKU
+    $_SESSION["MongoID"] = 0
+    header("Location: consultas/perfil.php");
   } else {
     header("Location: http://twitter.com/");
   }
