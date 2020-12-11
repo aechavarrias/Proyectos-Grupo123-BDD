f1043ef87d373c1a529e7ec94dc3235d3a7c97eb
JuanGalilea <juangk93@gmail.com>
2020-12-11 16:46:31 -0300

    request a API

--- Sites/ingreso/ingresar.php
+++ Sites/ingreso/ingresar.php
@@ -15,9 +15,28 @@
   if ($user == $usuario[0][4] and $pass == $usuario[0][6]) {
     $_SESSION["loggged"] = TRUE;
     $_SESSION["username"] = $usuario[0][1];
+
+    $data = array(
+        'nombre' => $usuario[0][1],
+    );
+
+    $options = array(
+        'http' => array(
+        'method'  => 'GET',
+        'content' => json_encode( $data ),
+        'header'=>  "Content-Type: application/json\r\n" .
+                    "Accept: application/json\r\n"
+        )
+    );
+    
+    $context  = stream_context_create( $options );
+    $result = file_get_contents( 'https://prometo-que-es-la-ultima.herokuapp.com/users/nombre', false, $context );
+    $response = json_decode($result, true);
+    echo "response = $response";
+
     // WEVIAR A MONGO / HEROKU
     $_SESSION["MongoID"] = 0;
-    header("Location: ../ingreso/perfil.php");
+    // header("Location: ../ingreso/perfil.php");
   } else {
     header("Location: http://twitter.com/");
   }
