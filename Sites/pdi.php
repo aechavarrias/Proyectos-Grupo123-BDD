c0836b9b2d39a713b88c8e178d77f168dd8c1af3
Alfredo <aechavarria@uc.cl>
2020-12-11 19:25:52 -0300

    prueba

--- Sites/pdi.php
+++ Sites/pdi.php
@@ -9,7 +9,7 @@
 <br/><br/>
   <h1 align="center">Bienvenid@ <?php echo $_SESSION["username"]; ?> al centro especial de la PDI?  </h1>
   <div class="container">
-  <form action="/mapa.php" method="post">
+  <form action="../mapa.php" method="post">
     <div class="text-center">
         <div id="success"></div>
         <button class="btn btn-primary btn-xl text-uppercase" id="inbox" value="inbox" type="submit">Ver Mapa</button>
